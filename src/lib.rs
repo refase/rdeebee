e56c79140346b8d403c2e661c9a3d39a79ab420f
Ratnadeep Bhattacharya <ratnadeep.bhattacharya1983@gmail.com>
2022-10-20 08:07:44 -0500

    +dangerous lint

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
-mod storage;
+// This is a dangerous lint.
+// Just here till we develop things fully.
+// Should be removed.
+#![allow(dead_code)]
 mod recovery;
+mod storage;
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
 }
